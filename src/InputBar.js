219b4d69b0c0b3d6013b7c43bcb7418dcc984578
kingliujiaxin <kingliujiaxin@gmail.com>
2017-03-19 16:32:11 +0800

    try to write my own autocomplete component.

--- src/InputBar.js
+++ src/InputBar.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
-import AutoCompleteGoogle from 'react-google-autocomplete';
-import TextField from 'material-ui/TextField';
-
-
+import $ from 'jquery';
 
 class InputBar extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(google);
+
+  }
 
   render() {
     return (
-      <div>        
-        <AutoCompleteGoogle 
-        style={{'width':'50%'}}
-        types={['geocode']} 
-        onPlaceSelected={(city)=>this.props.handleSearch(city)}
-        />
-      </div>
-    );
+      <div></div>
+    )
   }
 }
 
 export default InputBar;
 
-
-{/*<AutoComplete
-          hintText="New York, San Francisco, Beijing...."
-          dataSource={this.state.dataSource}
-          onUpdateInput={this.handleUpdateInput}
-          floatingLabelText="Type any city you want to know weather"
-          fullWidth={true}
-          onNewRequest={(city)=>this.props.handleSearch(city)}          
-        />*/}
